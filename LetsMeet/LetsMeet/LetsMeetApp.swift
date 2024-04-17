1d5cc275eef967c45547a213296e384df043be4f
Shrabonty Biswas <shrabontybiswas@gmail.com>
2024-04-17 20:00:59 +0200

    Project init

---
+++ LetsMeet/LetsMeet/LetsMeetApp.swift
@@ -0,0 +1,17 @@
+//
+//  LetsMeetApp.swift
+//  LetsMeet
+//
+//  Created by Shrabonty Biswas on 17.04.24.
+//
+
+import SwiftUI
+
+@main
+struct LetsMeetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
